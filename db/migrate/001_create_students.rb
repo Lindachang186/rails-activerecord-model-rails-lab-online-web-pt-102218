6e4a96931470bcc349618e57e756634682ecd8a8
Linda Chang <lindachang186@gmail.com>
2019-03-20 02:06:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,8 @@ class CreateStudents < ActiveRecord::migration
   def change
     create_table :students do |t|
       t.string :first_name
-      t.string :last_name 
+      t.string :last_name
     end
+  end 
 
 end
